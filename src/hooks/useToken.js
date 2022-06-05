506303f8d873ce6dbce32f221416b5616b421273
Intaek H <ahiou1ahiou@gmail.com>
2022-06-05 19:49:17 +0900

    style: 라이브러리 import 와 로컬 import 구분했습니다.

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -1,5 +1,6 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import IntegrationService from "../services/integration";
 
 const Storage = new IntegrationService(AsyncStorage);
